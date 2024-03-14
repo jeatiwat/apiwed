fe31a1be9f08328c6c908035d2fc69b86fce43b4
jeatiwat <jeleatiwad@gmail.com>
2024-03-15 02:47:29 +0700

    up

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import { app } from "./app";
 
-const port = process.env.port || 3000;
+const port = process.env.port || 4000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
